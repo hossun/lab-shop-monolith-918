42daef55058a398024375061393c168f4d151063
경호선 <hossun73@gmail.com>
2022-12-26 02:35:33 +0000

    수정

--- monolith/src/main/java/labshopmonolith/domain/Order.java
+++ monolith/src/main/java/labshopmonolith/domain/Order.java
@@ -57,8 +57,8 @@ public class Order  {
 
         labshopmonolith.external.DecreaseStockCommand decreaseStockCommand = new labshopmonolith.external.DecreaseStockCommand();
         // mappings goes here
-        MonolithApplication.applicationContext.getBean(labshopmonolith.external.InventoryService.class)
-            .decreaseStock(/* get???(), */ decreaseStockCommand);
+       // MonolithApplication.applicationContext.getBean(labshopmonolith.external.InventoryService.class).
+       //     .decreaseStock(/* get???(), */ decreaseStockCommand);
 
 
 
